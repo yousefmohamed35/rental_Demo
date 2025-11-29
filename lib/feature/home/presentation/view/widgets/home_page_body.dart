852f9b7cffb16865977853047b6de84e14896110
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-11-29 16:43:01 +0200

    create package info card

--- lib/feature/home/presentation/view/widgets/home_page_body.dart
+++ lib/feature/home/presentation/view/widgets/home_page_body.dart
@@ -5,6 +5,7 @@ import 'package:demorental/core/services/core_utilis/screen_util.dart';
 import 'package:demorental/core/utilities/light_theme/light_colors.dart';
 import 'package:flutter/material.dart';
 import 'package_info.dart';
+import 'package_info_card.dart';
 
 class HomePageBody extends StatelessWidget {
   const HomePageBody({super.key});
@@ -16,39 +17,10 @@ class HomePageBody extends StatelessWidget {
         children: [
           ArkanzaxLogo(),
           20.toHeight.verticalSpace,
-          Card(
-            color: AppColors.divider,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Text('Enterprise'),
-                  20.toHeight.verticalSpace,
-                  PackageInfo(
-                    title: 'المحاولات',
-                    amount: '2 / 3',
-                    progress: 0.7,
-                  ),
-                  20.toHeight.verticalSpace,
-                  PackageInfo(
-                    title: 'التذكيرات',
-                    amount: '2 / 8',
-                    progress: 0.2,
-                  ),
-                  20.toHeight.verticalSpace,
-                  AppButton(
-                    padding: EdgeInsets.all(0),
-                    label: 'upgrade plan',
-                    onPressed: () {},
-                    height: 40.toHeight,
-                    width: double.infinity,
-                  ),
-                ],
-              ),
-            ),
-          ),
+          PackageInfoCard(),
         ],
       ),
     );
   }
 }
+
---
+++ lib/feature/home/presentation/view/widgets/package_info_card.dart
@@ -0,0 +1,48 @@
+import 'package:demorental/core/services/core_utilis/screen_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../../core/presentation/widgets/button/app_button.dart';
+import '../../../../../core/utilities/light_theme/light_colors.dart';
+import 'package_info.dart';
+
+class PackageInfoCard extends StatelessWidget {
+  const PackageInfoCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.divider,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text('Enterprise'),
+            20.toHeight.verticalSpace,
+            PackageInfo(
+              title: 'المحاولات',
+              amount: '2 / 3',
+              progress: 0.7,
+            ),
+            20.toHeight.verticalSpace,
+            PackageInfo(
+              title: 'التذكيرات',
+              amount: '2 / 8',
+              progress: 0.2,
+            ),
+            20.toHeight.verticalSpace,
+            AppButton(
+              padding: EdgeInsets.all(0),
+              label: 'upgrade plan',
+              onPressed: () {},
+              height: 40.toHeight,
+              width: double.infinity,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
